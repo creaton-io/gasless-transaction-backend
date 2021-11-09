e3f23ce7773a98a011d3252a1d411f3c13225b61
Aleix <nigeon@gmail.com>
2021-11-09 16:34:35 +0100

    Tested and looks like working

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,21 @@ import { Router } from 'express';
 import { ethers, Contract } from "ethers";
 import erc20 from './abi/erc20.json';
 import { BaseProvider } from '@ethersproject/providers';
+import axios from 'axios';
 
 const routes = Router();
 
-routes.get('/gasless/:walletAddress/:creatorContractAddress', async (req, res) => {
+routes.post('/gasless/:walletAddress/:creatorContractAddress', async (req, res) => {
   const tokenAddress: string = process.env.TOKEN_ADDRESS || '';
   const threshold: string = process.env.TOKEN_AMOUNT_THRESHOLD || '0';
   const walletAddress: string = req.params.walletAddress;
   const creatorContractAddress: string = req.params.creatorContractAddress;
+  const signedMessage: string = req.body.signedMessage;
+
+  const recoveredSigner = ethers.utils.verifyMessage(`Creaton: Enabling gasless transactions for ${walletAddress}`, signedMessage);
+  if(recoveredSigner != walletAddress){
+    return res.json({error: "Signature doesn't match the sender"});
+  }
 
   try {
     ethers.utils.getAddress(tokenAddress);
@@ -24,37 +31,62 @@ routes.get('/gasless/:walletAddress/:creatorContractAddress', async (req, res) =
   const contract: Contract = new ethers.Contract(tokenAddress, erc20.abi, provider);
   const balance: string = (await contract.balanceOf(walletAddress)).toString();
 
-  if(balance > threshold){
+  if(balance >= threshold){
     return res.json(addGasless(creatorContractAddress));
+  }else{
+    return res.json(removeGasless(creatorContractAddress));
   }
-
-  return res.json({balance, gassless: (balance > threshold)});
 });
 
 async function addGasless(creatorContractAddress: string) {
-    const addMethodData = {
-      apiType: 'native',
-      methodType: 'write',
-      name: 'upload' + creatorContractAddress.slice(2, 6),
-      contractAddress: creatorContractAddress,
-      method: 'upload',
-    };
+  const addMethodData = {
+    apiType: 'native',
+    methodType: 'write',
+    name: 'upload' + creatorContractAddress.slice(2, 6),
+    contractAddress: creatorContractAddress,
+    method: 'upload',
+  };
 
-    const response = await fetch('https://api.biconomy.io/api/v1/meta-api/public-api/addMethod', {
-      method: 'POST', // or 'PUT'
+  const response = await axios.post('https://api.biconomy.io/api/v1/meta-api/public-api/addMethod', 
+    new URLSearchParams(addMethodData),
+    {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         authToken: process.env.BICONOMY_AUTH,
         apiKey: process.env.BICONOMY_API,
       },
-      body: new URLSearchParams(addMethodData),
-    });
-
-    if(!response.ok){
-      return {error: `An error has occured: ${response.status}`};
     }
+  );
 
-    return response.json();
+  if(response.status != 200){
+    return {error: `An error has occured: ${response.statusText}`};
   }
+
+  return response.data;
+}
+
+async function removeGasless(creatorContractAddress: string) {
+  const addMethodData = {
+    contractAddress: creatorContractAddress,
+    method: 'upload',
+  };
+
+  const response = await axios.delete('https://api.biconomy.io/api/v1/meta-api/public-api/deleteMethod', 
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        authToken: process.env.BICONOMY_AUTH,
+        apiKey: process.env.BICONOMY_API,
+      },
+      data: new URLSearchParams(addMethodData),
+    }
+  );
+
+  if(response.status != 200){
+    return {error: `An error has occured: ${response.statusText}`};
+  }
+
+  return response.data;
+}
 
 export default routes;
