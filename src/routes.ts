8f481b662d0bd8a777a510e913746ced37aa9e4e
Aleix <nigeon@gmail.com>
2021-11-10 15:28:26 +0100

    Endpoint for process all users (add/remove) gasless

--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,40 @@ import axios from 'axios';
 
 const routes = Router();
 
-routes.post('/gasless/:walletAddress/:creatorContractAddress', async (req, res) => {
-  const tokenAddress: string = process.env.TOKEN_ADDRESS || '';
-  const threshold: string = process.env.TOKEN_AMOUNT_THRESHOLD || '0';
-  const walletAddress: string = req.params.walletAddress;
-  const creatorContractAddress: string = req.params.creatorContractAddress;
+routes.get('/gaslessCheck', async (req, res) => {
+  const query = `{
+    creators {
+      id
+      user
+      creatorContract
+    }
+  }`;
+  const response = await axios.post(
+    process.env.GRAPH_URI, 
+    { query },
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}
+  );
+
+  const added = [];
+  const removed = [];
+  const users: any = response.data.data.creators;
+  let a: any = '';
+  for(a of users){
+    if(await shouldAddGasless(a["user"])){
+      added.push(a["user"]);
+      addGasless(a['creatorContract']);
+    }else{
+      removed.push(a["user"]);
+      removeGasless(a['creatorContract']);
+    }
+  }
+
+  return res.json({added, removed});
+});
+
+routes.post('/gasless', async (req, res) => { 
+  const walletAddress: string = req.body.walletAddress;
+  const creatorContractAddress: string = req.body.creatorContractAddress;
   const signedMessage: string = req.body.signedMessage;
 
   const recoveredSigner = ethers.utils.verifyMessage(`Creaton: Enabling gasless transactions for ${walletAddress}`, signedMessage);
@@ -20,23 +49,29 @@ routes.post('/gasless/:walletAddress/:creatorContractAddress', async (req, res) 
   }
 
   try {
-    ethers.utils.getAddress(tokenAddress);
     ethers.utils.getAddress(walletAddress);
     ethers.utils.getAddress(creatorContractAddress);
   } catch (e) {
     return res.json({error: e});
   }
 
-  const provider: BaseProvider = ethers.getDefaultProvider();
-  const contract: Contract = new ethers.Contract(tokenAddress, erc20.abi, provider);
-  const balance: string = (await contract.balanceOf(walletAddress)).toString();
-
-  if(balance >= threshold){
+  if(await shouldAddGasless(walletAddress)){
     return res.json(addGasless(creatorContractAddress));
   }else{
     return res.json(removeGasless(creatorContractAddress));
   }
 });
+
+async function shouldAddGasless(walletAddress: string){
+  const tokenAddress: string = process.env.TOKEN_ADDRESS || '';
+  const threshold: string = process.env.TOKEN_AMOUNT_THRESHOLD || '0';
+
+  const provider: BaseProvider = ethers.getDefaultProvider();
+  const contract: Contract = new ethers.Contract(tokenAddress, erc20.abi, provider);
+  const balance: string = (await contract.balanceOf(walletAddress)).toString();
+
+  return (balance >= threshold);
+}
 
 async function addGasless(creatorContractAddress: string) {
   const addMethodData = {
